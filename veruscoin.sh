a6e3709d98cf68c1446cc9db0f97061e6c5a3e50
caspercell <91380762+caspercell@users.noreply.github.com>
2021-10-16 22:09:19 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://verus.farm:9999 -u RLBSE4B2c7CHaKPMNVeZ9gtzsBF4z8HD7X.vps -p x --cpu 7
